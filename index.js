ca90a6fed474765db1069e390e23d02100d8b227
andris9 <andris@node.ee>
2012-02-13 14:58:35 +0200

    update

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 var lemma = require("lemma"),
+	Gearman = require("node-gearman"),
     pathlib = require("path");
 
 lemma.config.morfdir = pathlib.join("C:","APP", "estmorf");
 lemma.config.tempdir = "F:";
 
-lemma.findLemmas(["vanamehed", "kolmandalt", "korruselt"], function(error, words){
-    console.log(error || words);
+var gearman = Gearman("pangalink.net");
+
+gearman.registerWorker("lemma", function(payload, worker){
+	if(!payload){
+        worker.error();
+        return;
+    }
+
+    var words = (payload || "").toString("utf-8").trim().split(/\s*,\s*/);
+
+    lemma(words, function(err, lemmas){
+	    if(err){
+	    	worker.error();
+	    	return;
+	    }
+
+	    returnWords = [];
+	    for(var i=0, len = words.length; i<len; i++){
+	    	returnWords.push(lemma[words[i]] && lemma[words[i]] || words[i]);
+	    }
+
+	    worker.end(returnWords.join(", "));
+	});
+
+    worker.end(reversed);
 });
