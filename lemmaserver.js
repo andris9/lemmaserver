468815edeb7486005f95176b89af0cfeeaf9bc94
Andris Reinman <andris@node.ee>
2012-02-16 11:44:19 +0200

    update

--- lemmaserver.js
+++ lemmaserver.js
@@ -4,14 +4,16 @@ var lemma = require("lemma"),
     argv = require('optimist').argv;
 
 if(argv.h){
-    console.log("Usage: node lemmaserver.js --temp=[DIR] --morf=[DIR]");
+    console.log("Usage: node lemmaserver.js --temp=[DIR] --morf=[DIR] --gm=[DOMAIN] --port=4730");
     process.exit();
 }
 
 lemma.config.morfdir = argv.morf || pathlib.join("C:","APP", "estmorf");
 lemma.config.tempdir = argv.temp || "F:";
 
-var gearman = new Gearman("pangalink.net");
+var gearmanHost = argv.gm || "pangalink.net";  
+
+var gearman = new Gearman(gearmanHost);
 
 gearman.registerWorker("lemma", function(payload, worker){
 	if(!payload){
